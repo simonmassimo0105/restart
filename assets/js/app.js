b42c4c9ef9b94e04e6e9aa0aa1bea834dbb02476
間下善貴 <y.mashimo0105@gmail.com>
2019-12-16 14:19:24 +0900

    restart

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.dropdwn li').hover(function(){
+        $("ul:not(:animated)", this).slideDown();
+    }, function(){
+        $("ul.dropdwn_menu", this).slideUp();
+    });
+});
+
